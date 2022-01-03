de12ab8219d373499f3ea4ae4a30021acefc8276
Lackner Reinhard <reinhard.lackner@grawe.at>
2022-01-03 21:31:59 +0100

    Add empty Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GmailContactsConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
